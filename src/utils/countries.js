0587a705902f62e3ca0678ffbfae3661cc85a0b4
Colin Gillespie <ratchetandcolin@gmail.com>
2020-07-03 02:30:39 -0400

    Add countries - list and object of countries

---
+++ src/utils/countries.js
@@ -0,0 +1,29 @@
+export const countriesList = [
+    {
+        value: 'ca',
+        label: 'Canada',
+    },
+    {
+        value: 'mx',
+        label: 'Mexico',
+    },
+    {
+        value: 'us',
+        label: 'United States',
+    },
+];
+
+export const countriesObject = {
+    ca: {
+        value: 'ca',
+        label: 'Canada',
+    },
+    mx: {
+        value: 'mx',
+        label: 'Mexico',
+    },
+    us: {
+        value: 'us',
+        label: 'United States',
+    },
+};
