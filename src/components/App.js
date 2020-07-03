d63cfb283370877f4548aa55c208f1f5a5778988
Colin Gillespie <ratchetandcolin@gmail.com>
2020-07-03 02:29:09 -0400

    Add App state and prop drill

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Grid, CssBaseline } from '@material-ui/core';
 import Header from './Header';
 import MainContent from './MainContent';
 
 const App = () => {
+    const [formValues, setFormValues] = useState({
+        firstName: '',
+        lastName: '',
+        email: '',
+        address: '',
+        country: 'ca',
+        city: '',
+        postalCode: '',
+        cellPhone: '',
+        businessPhone: '',
+    });
     return (
         <>
             <CssBaseline />
             <Grid container direction="column">
                 <Grid item>
-                    <Header />
+                    <Header formValues={formValues} setFormValues={setFormValues} />
                 </Grid>
                 <Grid item container>
                     <Grid item xs={false} sm={2} />
                     <Grid item xs={12} sm={8}>
-                        <MainContent />
+                        <MainContent
+                            formValues={formValues}
+                            setFormValues={setFormValues}
+                        />
                     </Grid>
                     <Grid item xs={false} sm={2} />
                 </Grid>
