5075b72558a32ce80fe7b43cedcedefd5fdb873f
Colin Gillespie <ratchetandcolin@gmail.com>
2020-07-02 20:04:25 -0400

    Create general app structure

---
+++ src/components/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Grid, CssBaseline } from '@material-ui/core';
+import Header from './Header';
+import MainContent from './MainContent';
+
+const App = () => {
+    return (
+        <>
+            <CssBaseline />
+            <Grid container direction="column">
+                <Grid item>
+                    <Header />
+                </Grid>
+                <Grid item container>
+                    <Grid item xs={false} sm={2} />
+                    <Grid item xs={12} sm={8}>
+                        <MainContent />
+                    </Grid>
+                    <Grid item xs={false} sm={2} />
+                </Grid>
+            </Grid>
+        </>
+    );
+};
+
+export default App;
