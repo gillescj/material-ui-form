2726a95314b7c52a90c51d4b6caa1612750b6a41
Colin Gillespie <ratchetandcolin@gmail.com>
2020-07-06 00:44:15 -0400

    Add Pagination component

--- src/components/MainContent.js
+++ src/components/MainContent.js
@@ -1,19 +1,11 @@
 import React, { useState } from 'react';
-import { Container, Grid, Button } from '@material-ui/core';
-import { makeStyles } from '@material-ui/core/styles';
+import { Container } from '@material-ui/core';
 import FormAccountInfo from './FormAccountInfo';
 import FormLocationInfo from './FormLocationInfo';
 import FormConfirmation from './FormConfirmation';
-
-const useStyles = makeStyles((theme) => ({
-    pagination: {
-        marginTop: theme.spacing(2),
-    },
-}));
+import Pagination from './Pagination';
 
 const MainContent = ({ formValues, setFormValues }) => {
-    const classes = useStyles();
-
     const [page, setPage] = useState(1);
 
     const renderFormPage = () => {
@@ -44,34 +36,18 @@ const MainContent = ({ formValues, setFormValues }) => {
         }
     };
 
+    const previousPage = () => {
+        setPage((previousPage) => previousPage - 1);
+    };
+
+    const nextPage = () => {
+        setPage((previousPage) => previousPage + 1);
+    };
+
     return (
         <Container maxWidth="xs">
             {renderFormPage()}
-
-            <Grid container spacing={2} className={classes.pagination}>
-                <Grid item xs={12} sm={6}>
-                    <Button
-                        fullWidth
-                        variant="contained"
-                        color="secondary"
-                        disabled={page <= 1}
-                        onClick={() => setPage((previousPage) => previousPage - 1)}
-                    >
-                        Previous
-                    </Button>
-                </Grid>
-                <Grid item xs={12} sm={6}>
-                    <Button
-                        fullWidth
-                        variant="contained"
-                        color="primary"
-                        disabled={page >= 3}
-                        onClick={() => setPage((previousPage) => previousPage + 1)}
-                    >
-                        Next
-                    </Button>
-                </Grid>
-            </Grid>
+            <Pagination page={page} previousPage={previousPage} nextPage={nextPage} />
         </Container>
     );
 };
---
+++ src/components/Pagination.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import { Grid, Button } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+    pagination: {
+        marginTop: theme.spacing(2),
+    },
+}));
+
+const Pagination = ({ formId, page, previousPage, nextPage }) => {
+    const classes = useStyles();
+    return (
+        <Grid container spacing={2} className={classes.pagination}>
+            <Grid item xs={12} sm={4}>
+                <Button
+                    fullWidth
+                    variant="contained"
+                    color="secondary"
+                    disabled={page <= 1}
+                    onClick={() => previousPage()}
+                >
+                    Previous
+                </Button>
+            </Grid>
+            <Grid item xs={12} sm={8}>
+                <Button
+                    fullWidth
+                    variant="contained"
+                    color="primary"
+                    disabled={page >= 3}
+                    onClick={() => nextPage()}
+                >
+                    Next
+                </Button>
+            </Grid>
+        </Grid>
+    );
+};
+
+export default Pagination;
