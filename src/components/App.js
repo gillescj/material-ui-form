f9c9bf7dec59de6009345e717fdb862bc5e5c08e
Colin Gillespie <ratchetandcolin@gmail.com>
2020-07-06 02:37:47 -0400

    Remove unused import

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Grid, CssBaseline } from '@material-ui/core';
 import Header from './Header';
 import MainContent from './MainContent';
