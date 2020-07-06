579988825037870701c797c564696995fb0f7ed6
Colin Gillespie <ratchetandcolin@gmail.com>
2020-07-06 04:01:39 -0400

    Remove Form Reset Buttom, Add page selection

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,39 +1,66 @@
 import React, { useContext } from 'react';
-import { AppBar, Toolbar, Typography, Button, makeStyles } from '@material-ui/core';
+import {
+    AppBar,
+    Toolbar,
+    Typography,
+    Container,
+    Grid,
+    Button,
+    makeStyles,
+} from '@material-ui/core';
 import StoreContext from 'StoreContext';
 
 const useStyles = makeStyles((theme) => ({
     title: {
         flexGrow: 1,
+        display: 'none',
+        [theme.breakpoints.up('sm')]: {
+            display: 'block',
+        },
+    },
+    pageSelect: {
+        justifyContent: 'center',
+        textAlign: 'center',
+        [theme.breakpoints.up('sm')]: {
+            justifyContent: 'flex-end',
+            marginRight: theme.spacing(5),
+        },
     },
 }));
 
 const Header = () => {
     const classes = useStyles();
 
-    const { setFormValues } = useContext(StoreContext);
-
-    const handleResetClick = () => {
-        setFormValues({
-            firstName: '',
-            lastName: '',
-            email: '',
-            address: '',
-            country: 'ca',
-            city: '',
-            postalCode: '',
-            cellPhone: '',
-            businessPhone: '',
-        });
-    };
+    const { setPage } = useContext(StoreContext);
 
     return (
         <AppBar position="static">
             <Toolbar>
-                <Typography className={classes.title}>Material-UI Form</Typography>
-                <Button color="inherit" onClick={() => handleResetClick()}>
+                <Typography variant="h6" className={classes.title}>
+                    Material-UI Form
+                </Typography>
+                <Container maxWidth="xs">
+                    <Grid className={classes.pageSelect} container direction="row">
+                        <Grid item sm={2} xs={4}>
+                            <Button color="inherit" onClick={() => setPage(1)}>
+                                I
+                            </Button>
+                        </Grid>
+                        <Grid item sm={2} xs={4}>
+                            <Button color="inherit" onClick={() => setPage(2)}>
+                                II
+                            </Button>
+                        </Grid>
+                        <Grid item sm={2} xs={4}>
+                            <Button color="inherit" onClick={() => setPage(3)}>
+                                III
+                            </Button>
+                        </Grid>
+                    </Grid>
+                </Container>
+                {/* <Button color="inherit" onClick={() => handleResetClick()}>
                     Reset
-                </Button>
+                </Button> */}
             </Toolbar>
         </AppBar>
     );
